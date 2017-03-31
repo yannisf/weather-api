2a0485597b74712fe996cef1ae3c2abfb9bf92ef
yannis <yannis@ianos>
2017-03-31 18:33:35 +0300

    wip

--- parse_city_list.py
+++ parse_city_list.py
@@ -1,3 +1,29 @@
+import json
+
+cities = []
+
 with open('city.list.json', 'r') as data:
     for line in data:
-        print(line)
+        city = json.loads(line)
+        cities.append(city)
+
+greek_towns = []
+for city in cities:
+    if city['country'] == 'GR':
+        coord = (city['coord']['lat'],city['coord']['lon'])
+        greek_towns.append(coord)
+
+for coords in greek_towns:
+    print (coords)
+
+maxs = map(max, zip(*coords))
+mins = map(min, zip(*coords))
+
+print (maxs)
+print (mins)
+
+# top_left = (mins[0],maxs[1])
+# bottom_left = (mins[0],mins[1])
+# top_right = (maxs[0],maxs[1])
+# bottom_right = (maxs[0],mins[1])
+
